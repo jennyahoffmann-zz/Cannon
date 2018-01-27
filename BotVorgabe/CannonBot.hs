b291171ddbb84d27f7e4fe8b093bca06743540b8
Jenny Berger <jenny.a.berger@gmail.com>
2018-01-27 11:26:29 +0100

    Add iteration through row for straight forward move for white

--- BotVorgabe/CannonBot.hs
+++ BotVorgabe/CannonBot.hs
@@ -14,13 +14,14 @@ import Util
 -- IN: "4W5/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w///b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/7B2 w"
 -- OUT: "[g3-f4,...]"
 
-getExampleMovesList :: String
-getExampleMovesList = "[f3-e4,g2-e2]"
+-----------------------------------------------
 
 getMove :: String -> String
 getMove moves =
-    let move:rest = splitOn "," getExampleMovesList in
+    let move:rest = splitOn "," (listMoves getBoard) in
     removeFirstElement move
+
+-----------------------------------------------
 
 removeFirstElement :: String -> String
 removeFirstElement "" = ""
@@ -45,31 +46,7 @@ extendNumber :: Int -> String
 extendNumber 0 = ""
 extendNumber x = "1" ++ extendNumber (x-1)
 
-----------------------------------
-
-getBoard :: String
-getBoard = "111111111w/9W"
---getBoard = "ww1w1w1w1w/4W5"
-
-listMoves :: String -> String
-listMoves y =
-    let cBoard = splitOn "/" (convertBoardString getBoard) in
-    "[" ++ removeLastElement(findMoves cBoard) ++ "]"
-
-findMoves :: [String] -> String
-findMoves = findForwardMoves
-
-findForwardMoves :: [String] -> String
-findForwardMoves (row1:row2:rest) = findForwardMove row1 row2 9 8
-
-findForwardMove :: String -> String -> Int -> Int -> String
-findForwardMove "" _ _ _ = ""
-findForwardMove _ "" _ _ = ""
-findForwardMove (start:restRow1) (target:restRow2) startRow targetRow = convertMove (targetEmpty start target startRow targetRow 0 0)
-
-targetEmpty :: Char -> Char -> Int -> Int -> Int -> Int -> (Int, Int, Int, Int)
-targetEmpty 'w' '1' startRow targetRow startColumn targetColumn = (startColumn, startRow, targetColumn, targetRow)
-targetEmpty _ _ _ _ _ _ = (20,20,20,20)
+-----------------------------------------------
 
 convertMove :: (Int, Int, Int, Int) -> String
 convertMove (startColumn, startRow, targetColumn, targetRow) = getValidMove (convertColumn startColumn ++ convertRow startRow ++ "-" ++ convertColumn targetColumn ++ convertRow targetRow ++ ",")
@@ -96,4 +73,30 @@ convertColumn 9 = "j"
 convertColumn 20 = ""
 convertColumn _ = "column out of area"
 
--------------------------
+-----------------------------------------------
+
+getBoard :: String
+--getBoard = "111111111w/9W"
+getBoard = "ww1w1w1w1w/4W5"
+
+listMoves :: String -> String
+listMoves y =
+    let cBoard = splitOn "/" (convertBoardString getBoard) in
+    "[" ++ removeLastElement(findMoves cBoard) ++ "]"
+
+findMoves :: [String] -> String
+findMoves = findForwardMoves
+
+findForwardMoves :: [String] -> String
+findForwardMoves (row1:row2:rest) = findForwardMove row1 0 9 row2 0 8
+
+-- row1 startColumn startRow row2 targetColumn targetRow
+findForwardMove :: String -> Int -> Int -> String -> Int -> Int -> String
+findForwardMove "" _ _ _ _ _ = ""
+findForwardMove _ _ _ "" _ _ = ""
+findForwardMove (start:restRow1) startColumn startRow (target:restRow2) targetColumn targetRow =
+  convertMove (targetEmpty start startColumn startRow target targetColumn targetRow) ++ findForwardMove restRow1 (startColumn+1) startRow restRow2 (targetColumn+1) targetRow
+
+targetEmpty :: Char -> Int -> Int -> Char -> Int -> Int -> (Int, Int, Int, Int)
+targetEmpty 'w' startColumn startRow '1' targetColumn targetRow = (startColumn, startRow, targetColumn, targetRow)
+targetEmpty _ _ _ _ _ _ = (20,20,20,20)
